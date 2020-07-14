3a8e1b4dc2aba2da711316233bfa579d44213e62
arifhossain123 <arifhossain42136@gmail.com>
2020-07-14 11:46:06 +0600

    first commit

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function Main() {
+    return (
+        <div>
+            <h1 className="text-center">welcome</h1>
+            <button className="btn btn-outline-success">click me</button>
+        </div>
+    )
+}
+
+export default Main
